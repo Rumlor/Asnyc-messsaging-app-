d4a11550208239a85f63eb9843ce598ddae6c16c
rumlor <rumlor61@gmail.com>
2022-09-22 07:37:28 +0300

    first parent app

---
+++ Server/src/test/java/com/sixtyone/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixtyone.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
